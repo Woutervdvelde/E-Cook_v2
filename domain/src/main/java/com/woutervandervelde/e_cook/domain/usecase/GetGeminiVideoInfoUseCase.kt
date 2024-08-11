4b1c6c2e57852c08eea9c401711160587f31411c
Wouter van der Velde <woutervandervelde2000@hotmail.com>
2024-08-11 19:24:33 +0200

    removed commented code

--- domain/src/main/java/com/woutervandervelde/e_cook/domain/usecase/GetGeminiVideoInfoUseCase.kt
+++ domain/src/main/java/com/woutervandervelde/e_cook/domain/usecase/GetGeminiVideoInfoUseCase.kt
@@ -10,15 +10,11 @@ object GetGeminiVideoInfoUseCase {
         val model = GenerativeModel.getModel()
         val response = model.generateContent(
             content {
-//                fileData(uri = "https://firebasestorage.googleapis.com/v0/b/e-cook-92f20.appspot.com/o/instagram_recipe_sample.mp4?alt=media&token=ae3dd994-f113-4aff-a595-71e0708f8542", mimeType = "video/mp4")
-//                fileData(videoInfo.thumbnail, mimeType = "image/jpg")
-                frames.forEach {
-                    image(it)
-                }
+                frames.forEach { image(it) }
                 text(videoInfo.description)
             }
         )
 
-        return response.text ?: "empty response"
+        return response.text ?: ""
     }
 }
