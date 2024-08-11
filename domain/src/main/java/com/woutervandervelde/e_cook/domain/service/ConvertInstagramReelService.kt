de3cf69995968b98679241d9b0ba9f795937c507
Wouter van der Velde <woutervandervelde2000@hotmail.com>
2024-08-11 21:49:00 +0200

    created initial foreground service

---
+++ domain/src/main/java/com/woutervandervelde/e_cook/domain/service/ConvertInstagramReelService.kt
@@ -0,0 +1,13 @@
+package com.woutervandervelde.e_cook.domain.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ConvertInstagramReelService: Service() {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+}
